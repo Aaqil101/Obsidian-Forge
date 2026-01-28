d9eb7c014e15f12d54a0ef42ed78988bf42614ce
Aaqil Ilyas <aaqililyas.101@protonmail.com>
2026-01-28 09:34:04 +0530

    Update QCheckBox.py

--- src/ui/styles/QCheckBox.py
+++ src/ui/styles/QCheckBox.py
@@ -1,13 +1,5 @@
-# ----- Core Modules-----
-from src.core import BORDER_RADIUS_SMALL
-
 # ----- Utils Modules-----
-from src.utils import (
-    COLOR_DARK_BLUE,
-    COLOR_LIGHT_BLUE,
-    THEME_BORDER,
-    THEME_TEXT_PRIMARY,
-)
+from src.utils import THEME_TEXT_PRIMARY
 
 
 def qss() -> str:
@@ -19,30 +11,9 @@ def qss() -> str:
     }}
 
     QCheckBox::indicator {{
-        width: 18px;
-        height: 18px;
-        border: 2px solid {THEME_BORDER};
-        border-radius: {BORDER_RADIUS_SMALL - 2}px;
-        background-color: rgba(255, 255, 255, 0.04);
-    }}
-
-    QCheckBox::indicator:hover {{
-        border: 2px solid rgba(122, 162, 247, 0.5);
-        background-color: rgba(255, 255, 255, 0.06);
-    }}
-
-    QCheckBox::indicator:checked {{
-        background-color: {COLOR_DARK_BLUE};
-        border: 2px solid {COLOR_DARK_BLUE};
-    }}
-
-    QCheckBox::indicator:checked:hover {{
-        background-color: {COLOR_LIGHT_BLUE};
-        border: 2px solid {COLOR_LIGHT_BLUE};
-    }}
-
-    QCheckBox::indicator:disabled {{
-        background-color: rgba(255, 255, 255, 0.02);
-        border: 2px solid rgba(255, 255, 255, 0.1);
+        width: 0px;
+        height: 0px;
+        border: none;
+        background: transparent;
     }}
     """
